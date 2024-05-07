c8a20c69cfea9b4b96d599ad7cf4355e4aa6bb73
LeifGunnar <leif.gunnar.erlandsen@nav.no>
2024-05-07 09:24:41 +0200

    first version

---
+++ db.js
@@ -0,0 +1,44 @@
+// db.js
+var postgres = require('postgres');
+
+const sql = postgres({ /* options */ }) // will use psql environment variables
+
+
+async function getUsersOver(age, cb) {
+    await sql`
+      select
+        name,
+        age
+      from users
+      where age > ${age}
+    `
+        .then((res) => {
+            console.log('sql', res);
+            return cb(null, JSON.stringify(res[0]));
+        })
+        .catch((error) => {
+            console.log('sql', error);
+            return cb('query failed: check log.')
+        });
+    // users = Result [{ name: "Walter", age: 80 }, { name: 'Murray', age: 68 }, ...]
+    // return users
+}
+
+
+async function insertUser({ name, age }) {
+    const users = await sql`
+      insert into users
+        (name, age)
+      values
+        (${name}, ${age})
+      returning name, age
+    `
+    // users = Result [{ name: "Murray", age: 68 }]
+    return users
+}
+
+module.exports = {
+    getUsersOver,
+    insertUser,
+    sql
+};
